ed675baaeb578246813ce613777c3a5097e3e0d4
wsf <wangsifeng@qiyuexinxi.com>
2018-09-03 14:47:05 +0800

    添加base和login

---
+++ loginmodule/src/main/java/playandroid/cmcc/com/loginmodule/MainActivity.java
@@ -0,0 +1,13 @@
+package playandroid.cmcc.com.loginmodule;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
