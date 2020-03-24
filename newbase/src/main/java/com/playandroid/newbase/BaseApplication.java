34305164bbf55be3556d44bbe9e270eaac437f7b
wsf <wsf123456789>
2020-03-24 15:42:24 +0800

    fix

--- newbase/src/main/java/com/playandroid/newbase/BaseApplication.java
+++ newbase/src/main/java/com/playandroid/newbase/BaseApplication.java
@@ -1,7 +1,9 @@
 package com.playandroid.newbase;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.blankj.utilcode.util.CrashUtils;
 import com.blankj.utilcode.util.Utils;
 
 public class BaseApplication extends Application {
@@ -13,9 +15,16 @@ public class BaseApplication extends Application {
         super.onCreate();
         baseApplication = this;
         Utils.init(this);
+        CrashUtils.init(new CrashUtils.OnCrashListener() {
+            @Override
+            public void onCrash(String crashInfo, Throwable e) {
+                Log.e("wsf", crashInfo);
+            }
+        });
     }
 
     public static BaseApplication getBaseApplication() {
         return baseApplication;
     }
+
 }
