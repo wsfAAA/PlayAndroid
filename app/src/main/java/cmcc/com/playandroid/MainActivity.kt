8e8a5657d04f7ca26d33871702e26bb4379760c1
wsf <17602115499@163.com>
2020-05-30 22:34:08 +0800

    fix

--- app/src/main/java/cmcc/com/playandroid/MainActivity.kt
+++ app/src/main/java/cmcc/com/playandroid/MainActivity.kt
@@ -2,11 +2,14 @@ package cmcc.com.playandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.setText("这是测试数据。。。。。。。。。。。。。。。。。")
+        image.setImageResource(R.mipmap.ic_launcher)
     }
 }
